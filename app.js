10ed49c4002fe74c53637f527c4e1dde4da0fdfc
Alex  Ross <alexross@Alexs-Mac-mini.local>
2025-02-04 22:49:16 -0500

    Connected Dadabase to the... dadabase!

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const jokeRouter = require("./routers/jokeRouts");
 
 const app = express();
-
 app.use("/api/jokes", jokeRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
+const database = process.env.DATABASE.replace(
+  "<db_password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(database)
+  .then(() => console.log("database connection successful"))
+  .catch((err) => {
+    console.error("Database connection error:", err.message);
+    process.exit(1);
+  });
 
 const port = 8000;
 const server = app.listen(port, () => {
