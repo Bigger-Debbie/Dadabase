cbdc29047e5dcbae7354bbfb419c252da4b1e68b
Alex  Ross <alexross@Alexs-Mac-mini.local>
2025-02-06 21:52:57 -0500

    Implemented createJoke controller and POST /api/jokes route

--- controllers/jokeController.js
+++ controllers/jokeController.js
@@ -1,6 +1,28 @@
-exports.getJoke = (req, res, next) => {
+const Joke = require("../models/jokeModel");
+
+exports.getJoke = async (req, res, next) => {
+  const randomJoke = await Joke.aggregate([{ $sample: { size: 1 } }])[0];
+
   res.status(200).json({
     status: "success",
-    data: "pull my finger...",
+    joke: randomJoke,
   });
+};
+
+exports.createJoke = async (req, res, next) => {
+  try {
+    const joke = await Joke.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      joke,
+    });
+  } catch (error) {
+    console.log("Validation error:", error);
+
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
 };
--- routers/jokeRouts.js
+++ routers/jokeRouts.js
@@ -3,6 +3,6 @@ const jokeController = require("../controllers/jokeController");
 
 const router = express.Router();
 
-router.route("/").get(jokeController.getJoke);
+router.route("/").get(jokeController.getJoke).post(jokeController.createJoke);
 
 module.exports = router;
