325c439878af4c993d35cd31e4b54612eaa96804
Alex  Ross <alexross@Alexs-Mac-mini.local>
2025-02-19 21:09:10 -0500

    Started the home view

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 
 exports.getHome = catchAsync(async (req, res, next) => {
-  res.status(200).render("home");
+  res.status(200).render("home", {
+    title: "Home",
+  });
 });
