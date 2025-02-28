2105fb2d2bd0dea223bd0cc931ae8bc6204ecfdb
Alex  Ross <alexross@Alexs-Mac-mini.local>
2025-02-27 19:37:45 -0500

    Started docs section - added open and close menu functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,21 @@
 const send_req = document.querySelector(".send_req");
+const categories = [
+  {
+    arrow: document.querySelector(".auth ion-icon"),
+    content: document.querySelector(".auth_content"),
+    isOpen: false,
+  },
+  {
+    arrow: document.querySelector(".jokes ion-icon"),
+    content: document.querySelector(".jokes_content"),
+    isOpen: false,
+  },
+  {
+    arrow: document.querySelector(".admin ion-icon"),
+    content: document.querySelector(".admin_content"),
+    isOpen: false,
+  },
+];
 
 async function getJoke() {
   const response = await fetch("http://127.0.0.1:8000/api/joke", {
@@ -13,6 +30,26 @@ async function getJoke() {
   return data;
 }
 
+function toggleCategory(category) {
+  categories.forEach((cat) => {
+    if (cat !== category) {
+      cat.arrow.style.transform = "rotate(0deg)";
+      cat.content.style.display = "none";
+      cat.isOpen = false;
+    }
+  });
+
+  if (category.isOpen) {
+    category.arrow.style.transform = "rotate(0deg)";
+    category.content.style.display = "none";
+    category.isOpen = false;
+  } else {
+    category.arrow.style.transform = "rotate(90deg)";
+    category.content.style.display = "block";
+    category.isOpen = true;
+  }
+}
+
 send_req.addEventListener("click", async (e) => {
   e.preventDefault();
 
@@ -20,4 +57,11 @@ send_req.addEventListener("click", async (e) => {
   const response = document.querySelector(".response_container pre code");
 
   response.textContent = JSON.stringify(joke, null, 2);
+});
+
+categories.forEach((category) => {
+  category.arrow.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleCategory(category);
+  });
 });
